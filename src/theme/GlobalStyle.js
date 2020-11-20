e138b6ae7b72e48bc7b758d7a671d941a89a5643
Mateusz Polok <mateusz24polok@gmail.com>
2020-11-20 19:56:03 +0100

    project has been pre-prepared for further jobs

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+    html{
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+    };
+
+    *,::after, ::before{
+        box-sizing: inherit
+    }
+`;
+
+export default GlobalStyle;
